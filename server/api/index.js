5616d808722a047c20e8003eec1c6051d5fa6dbf
John Shivers <johnshivers3@gmail.com>
2021-12-01 22:33:51 -0500

    revise

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,5 @@
 const router = require("express").Router()
 
 router.get("/api/")
+
+module.exports = router
--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const router = require("express").Router();
-const cors = require("cors");
-
+const apiRouter = require("./api");
 
 const app = express();
 const port = 8081;
@@ -10,9 +8,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-
+//
+app.use(apiRouter);
 app.get("/", (req, res) => {
   res.send("Success!!");
+});
+
+// Error Handler
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
 });
 
 app.listen(port, () => {
